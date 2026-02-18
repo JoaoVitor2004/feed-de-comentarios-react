9ee64d77f298a49413411d8e2100e2e294e55eab
JoaoVitor2004 <joaovitorsouzaa28122004@gmail.com>
2026-02-18 13:22:08 -0300

    Corrigir alguns bugs

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/globals.css'
 import App from './App.jsx'
-import Counter from './contexts/Counter/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Counter>
       <App />
-    </Counter>
   </StrictMode>
 )
